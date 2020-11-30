bd549544b0fc60cefe1b1a6790dd61438eea8467
et6341 <72764888+et6341@users.noreply.github.com>
2020-11-30 17:26:39 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+while True:
+    print("Current Room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) + "°F" + " - " + input.temperature(TemperatureUnit.CELSIUS) + "°C")
+    if input.temperature(TemperatureUnit.FAHRENHEIT) >= 81:
+        light.set_all(light.rgb(255,0,0))
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) <= 65:
+        light.set_all(light.rgb(0,255,0))
+    else:
+        light.set_all(light.rgb(0,0,255))
--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+while (true) {
+    console.log("Current Room Temperature: " + input.temperature(TemperatureUnit.Fahrenheit) + "°F" + " - " + input.temperature(TemperatureUnit.Celsius) + "°C")
+    if (input.temperature(TemperatureUnit.Fahrenheit) >= 81) {
+        light.setAll(light.rgb(255, 0, 0))
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) <= 65) {
+        light.setAll(light.rgb(0, 255, 0))
+    } else {
+        light.setAll(light.rgb(0, 0, 255))
+    }
+    
+}
